b0d5a0f82034f8e595cf387f40f44d859badd8c5
Onamar <jwsuarez@comcast.net>
2016-04-28 00:03:03 -0400

    Passwords

--- src/main.rkt
+++ src/main.rkt
@@ -9,12 +9,15 @@
          web-server/templates
          web-server/servlet
          web-server/servlet-env
+         web-server/http
+         parser-tools/lex
          xml)
 
 ;; Runtime declarations
 (define-runtime-path CURR_DIR ".")
 (define CURR_VERSION 0.01)
 (define FILE_VERSION_NAME "VERSION")
+(define root (path->string (current-directory)))
 
 (define (write-to-file path version_number)
   (call-with-output-file path
@@ -31,6 +34,24 @@
 
     ;; Else create file and set the value to whatever is current
     (write-to-file FILE_VERSION_NAME CURR_VERSION))
+
+; Path to password file:
+(define password-file (string-append root "/passwd.txt"))
+
+; returns true if any element of list matches pred
+(define (any? pred list)
+  (cond ((null? list) #f)
+        ((equal? pred (car list)) #t)
+        (else (any? pred (cdr list)))))
+                 
+; Password check
+; Checks if given username and password match any in the databse
+(define (credentials-valid? passwd-file username password)
+  (define lines (file->lines passwd-file)) ; Reads password file as a list of lines
+  (define (password-matches? line)
+    (and (any? username line)) ; Checks usernames
+         (any? password line)) ; Checks passwords
+(password-matches? lines))
 
 (define (fast-template
          title
@@ -50,7 +71,7 @@
                (list
                 (build-path CURR_DIR)))
 
-(define TimelineThing (initialize-timeline! "timeline.db"))
+;(define TimelineThing (initialize-timeline! "timeline.db"))
 ; (timeline-insert-post! TimelineThing "Test_Title" "Wow cool")
 ; (timeline-posts TimelineThing)
 
